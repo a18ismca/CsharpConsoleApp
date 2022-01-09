9bca8e99a08af38b4c9e6368cb96ae0573787ff3
a18ismca <a18ismca@student.his.se>
2022-01-09 13:41:55 +0100

    HelloWorld

--- ConsoleAppForGit/Program.cs
+++ ConsoleAppForGit/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppForGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
